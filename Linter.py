032769a84a27d308980cc05c74dbe1963f2ace89
ISich <arseny.voroni@yandex.ru>
2023-12-21 09:46:44 +0500

    Add anti_checker

--- Linter.py
+++ Linter.py
@@ -1,4 +1,4 @@
-def check_tabs(file_path, err, tabs):
+def check_tabs(file_path, err, block_lines, tabs):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     tab_count = 0
@@ -21,13 +21,13 @@ def check_tabs(file_path, err, tabs):
                 else:
                     st += 1
             line0 = "    " * tab_count + line[st:]
-        if line != line0 and len(line.strip()) != 0:
+        if line != line0 and len(line.strip()) != 0 and ind + 1 not in block_lines:
             err.write(f"Tab error in {ind+1} line\n")
 
         ind += 1
 
 
-def check_empty_lines(file_path, err, between_func, posible):
+def check_empty_lines(file_path, err, block_lines, between_func, posible):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     cur_emt = 0
@@ -42,10 +42,12 @@ def check_empty_lines(file_path, err, between_func, posible):
         else:
             if empty and cur_emt != 0:
                 for i in range(cur_emt):
-                    err.write(f"Empty string error in {i+1} line\n")
+                    if i + 1 not in block_lines:
+                        err.write(f"Empty string error in {i+1} line\n")
             elif cur_emt > posible:
                 for i in range(cur_emt - posible):
-                    err.write(f"Empty string error in {ind-cur_emt+i+1} line\n")
+                    if ind-cur_emt+i+1 not in block_lines:
+                        err.write(f"Empty string error in {ind-cur_emt+i+1} line\n")
             cur_emt = 0
             empty = False
 
@@ -53,7 +55,8 @@ def check_empty_lines(file_path, err, between_func, posible):
     if lines[-1][-1] == '\n':
         cur_emt += 1
     for i in range(cur_emt):
-        err.write(f"Empty string error in {len(lines)-cur_emt+i+2} line\n")
+        if len(lines)-cur_emt+i+2 not in block_lines:
+            err.write(f"Empty string error in {len(lines)-cur_emt+i+2} line\n")
 
 
 def check_space_line(line, space_el):
@@ -71,7 +74,7 @@ def check_space_line(line, space_el):
     return res
 
 
-def check_space(file_path, err, space_elements):
+def check_space(file_path, err, block_lines, space_elements):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     ind = 0
@@ -79,10 +82,11 @@ def check_space(file_path, err, space_elements):
         ind += 1
         line_err = check_space_line(line, space_elements)
         for error in line_err:
-            err.write(f"Space error in {ind} line {error} pos by element {line[error]}\n")
+            if ind not in block_lines:
+                err.write(f"Space error in {ind} line {error} pos by element {line[error]}\n")
 
 
-def check_lines_len(file_path, err, max_len):
+def check_lines_len(file_path, err, block_lines, max_len):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     ind = 0
@@ -91,7 +95,8 @@ def check_lines_len(file_path, err, max_len):
         if line.strip() == 0:
             continue
         if check_line_len(line, max_len):
-            err.write(f"{ind} line in too large: {len(line)} > {max_len}\n")
+            if ind not in block_lines:
+                err.write(f"{ind} line in too large: {len(line)} > {max_len}\n")
 
 
 def check_line_len(line, max_len):
@@ -99,11 +104,11 @@ def check_line_len(line, max_len):
         return True
     return False
 
-
-def linter_main(file_name, max_len_string, empty_lines, tabs_count):
+def linter_main(block_lines, file_name, max_len_string, empty_lines, tabs_count):
     space_elements = ['+', '-', '*', '/', '=']
     err = open("errors.txt", "w")
-    check_empty_lines(file_name, err, 2, empty_lines)
-    check_tabs(file_name, err, tabs_count)
-    check_space(file_name, err, space_elements)
-    check_lines_len(file_name, err, max_len_string)
+    err.write(f"Block lines is: {' '.join([str(i) for i in block_lines])}\n")
+    check_empty_lines(file_name, err, block_lines, 2, empty_lines)
+    check_tabs(file_name, err, block_lines, tabs_count)
+    check_space(file_name, err, block_lines, space_elements)
+    check_lines_len(file_name, err, block_lines, max_len_string)
--- Menu.py
+++ Menu.py
@@ -10,6 +10,7 @@ class Menu():
         self.string_len = int()
         self.empty_lines = int()
         self.tabs_count = int()
+        self.block_symbols = list()
         self.menu(self.window)
         self.mainloop(self.window)
 
@@ -29,8 +30,8 @@ class Menu():
         entry1 = Entry(frame1, width=8)
         entry1.insert(END, 30)
         entry1.pack(side='left', fill='x')
-        label1_2 = Label(frame1, text="Текст 1.2")
-        label1_2.pack(side='left')
+        #label1_2 = Label(frame1, text="Текст 1.2")
+        #label1_2.pack(side='left')
 
         frame2 = Frame(window)
         frame2.pack(fill='x', padx=5, pady=5)
@@ -39,8 +40,8 @@ class Menu():
         entry2 = Entry(frame2, width=8)
         entry2.insert(END, 1)
         entry2.pack(side='left', fill='x')
-        label2_2 = Label(frame2, text="Текст 1.2")
-        label2_2.pack(side='left')
+        #label2_2 = Label(frame2, text="Текст 1.2")
+        #label2_2.pack(side='left')
 
         frame3 = Frame(window)
         frame3.pack(fill='x', padx=5, pady=5)
@@ -49,11 +50,21 @@ class Menu():
         entry3 = Entry(frame3, width=8)
         entry3.insert(END, 1)
         entry3.pack(side='left', fill='x')
-        label3_2 = Label(frame3, text="Текст 1.2")
-        label3_2.pack(side='left')
+        #label3_2 = Label(frame3, text="Текст 1.2")
+        #label3_2.pack(side='left')
+
+        framel1 = Frame(window)
+        framel1.pack(fill='x', padx=5, pady=5)
+        labell1 = Label(framel1, text="Dont checking lines(write from space):")
+        labell1.pack(side='left')
+
+        framel2 = Frame(window)
+        framel2.pack(fill='x', padx=5, pady=5)
+        entryblock = Entry(framel2, width=50)
+        entryblock.pack(side='left', fill='x')
 
         result_button = Button(window, text="Result", font=("Roboto", 14), width=16,
-        command=lambda: self.__do_result(entry1.get(), entry2.get(), entry3.get()))
+        command=lambda: self.__do_result(entryblock.get(), entry1.get(), entry2.get(), entry3.get()))
         result_button.pack(side=BOTTOM, pady=10)
 
         btn_file = Button(window, text="Choose_file", font=("Roboto", 8), width=10,
@@ -95,10 +106,23 @@ class Menu():
                 return True
         return False
 
-    def __do_result(self, string_len, empty_lines, tabs_count):
+    def __check_block_symbols(self, value):
+        value = value.split()
+        res = True
+        for i in range(len(value)):
+            if value[i].isdigit():
+                value[i] = int(value[i])
+                if value[i] >= 0:
+                    continue
+            res = False
+            return res
+        self.block_symbols = value.copy()
+        return res
+
+    def __do_result(self, block_lines, string_len, empty_lines, tabs_count):
         if self.__check_entry_len(string_len) and self.__check_entry_lines(empty_lines) \
-                and self.__check_entry_tabs(tabs_count):
-            linter_main(self.file_path, self.string_len, self.empty_lines, self.tabs_count)
+                and self.__check_entry_tabs(tabs_count) and self.__check_block_symbols(block_lines):
+            linter_main(self.block_symbols, self.file_path, self.string_len, self.empty_lines, self.tabs_count)
             self.close_window(self.window)
             self.__show_res()
         else:
